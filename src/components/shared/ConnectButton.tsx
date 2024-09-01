7d243a050fb05d56fe658597d8d15009c1814f2e
Sarang130203 <sarangbagad38@gmail.com>
2024-09-02 01:59:05 +0530

    changes for connectbutton

--- src/components/shared/ConnectButton.tsx
+++ src/components/shared/ConnectButton.tsx
@@ -1,22 +1,9 @@
 "use client";
 
 import React, { useEffect } from 'react';
-import { usePathname, useRouter } from 'next/navigation';
-import { useAccount } from 'wagmi';
+
 
 const ConnectButton = () => {
-  const router = useRouter();
-  const pathname = usePathname()
-  const { isConnected } = useAccount();
-
-  useEffect(() => {
-    if (!isConnected) {
-      router.push('/');
-    } else if (pathname === '/') {
-      router.push('/memberDashboard');
-    }
-  }, [isConnected, router]);
-
   return (
     <div>
       <w3m-button />
